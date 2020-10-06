50838b9c5e75ef9b4201676a57af257dbd2c9c48
super-beam <56254035+super-beam@users.noreply.github.com>
2020-10-07 03:14:13 +0530

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ import pandas as pd
 # from sklearn.model_selection import train_test_split
 # from sklearn.feature_extraction.text import TfidfVectorizer
 # from sklearn.metrics import accuracy_score
-# import numpy as np
 # import itertools
 
 import pickle
